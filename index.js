a329f9687b3102b30af66eef6251a52f84886071
Joao Vitor Salomao <124266907+jvsalomao@users.noreply.github.com>
2023-12-06 20:31:26 -0300

    Adiciona o arquivo calculadora.js e trabalha com módulos no node

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 var validator = require("validator");
 
 console.log(validator.isEmail("foo@hotmail.com"));
+
+var calculadora = require("./calculadora.js");
+
+console.log(calculadora.somar(1, 2));
+console.log(calculadora.subtrair(1, 2));
+console.log(calculadora.multiplicar(1, 2));
+console.log(calculadora.dividir(1, 2));
